9c8777ea360b12000d8a724a8e9522ad4e516696
Steven Mwesigwa <stevekats1356@gmail.com>
2018-04-24 17:50:23 +0300

    Update portfolio.js

--- portfolio/steven-mwesigwa-portfolio-page/js/portfolio.js
+++ portfolio/steven-mwesigwa-portfolio-page/js/portfolio.js
@@ -2,8 +2,9 @@ $(document).ready(function () {
 	var $year = new Date().getFullYear(); 
 	$("#year").html($year);
 
-	$("header").click(()=> {
+	$("#maincontent").on( "click", ()=> {
 		$("hamburger").css({"background-color": "yellow", "font-size": "200%"});
+		alert("yes");
 	
 	});	
 
