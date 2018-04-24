bb25b19c50c5e16b73abfb41b6a6b1c0675787b8
Steven Mwesigwa <stevekats1356@gmail.com>
2018-04-24 17:42:49 +0300

    $("header").click(()

--- portfolio/steven-mwesigwa-portfolio-page/js/portfolio.js
+++ portfolio/steven-mwesigwa-portfolio-page/js/portfolio.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
 	var $year = new Date().getFullYear(); 
 	$("#year").html($year);
 
+	$("header").click(()=> {
+		$("hamburger").css({"background-color": "yellow", "font-size": "200%"});
 	
+	});	
 
 });
